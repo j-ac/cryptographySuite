eca7f2424820440e9291f191240b5a1d58555d6b
James Correia <jcorr851@gmail.com>
2021-05-03 20:37:34 -0400

    Added support for encryption and decryption into a Japanese character set, support for command line arguments
    removed prompts to the user for key data, instead requires command line arguments
    Outputs to japanese characters to make the encrypted text less decipherable.

--- cypherWheel.py
+++ cypherWheel.py
@@ -1,66 +1,69 @@
 #!/bin/python3
 import string
 import random
+import argparse
 
 def main():
-    alphabet = list(string.ascii_uppercase)
-    answer = input("Do you want to encrypt or decrypt? (E\D)")
+    parser = argparse.ArgumentParser()
+    action = parser.add_mutually_exclusive_group(required=True) #make encrypting and decrypting mutually exclusive operations.
+    action.add_argument("--encrypt", "-e", help="encrypts the message string", action="store_true")
+    action.add_argument("--decrypt", "-d", help="decrypts the message string", action="store_true")
+    parser.add_argument("WHEELS", nargs=4, type=int, metavar='N', help="integer position for the wheels in the cypher. Numbers divisible by 26 will not encrypt well")
+    parser.add_argument("MESSAGE", help="the message you want to encrypt or decrypt.")
+    args = parser.parse_args()
+    
+    if(args.decrypt):
+        decrypt(args.MESSAGE, args.WHEELS)
+    else:
+        encrypt(args.MESSAGE, args.WHEELS)
 
-    while not(answer == "E" or answer == "D"):
-        answer = input("Do you want to encrypt or decrypt? (E\D)")
-    if answer == "E":
-        encrypt()
-    if answer == "D":
-        decrypt()
+    
+def encrypt(message, offsets):
+    for i in range(len(offsets)):
+        offsets[i] = offsets[i] % ALPHABET_SIZE  
 
-def encrypt():
-    firstWheelOffset = int(input("Enter the offset for the first wheel, numbers divisible by 26 will not encrypt.")) % 26
-    secondWheelOffset = int(input("Enter the offset for the second wheel, numbers divisible by 26 will not encrypt.")) % 26
-    thirdtWheelOffset = int(input("Enter the offset for the third wheel, numbers divisible by 26 will not encrypt.")) % 26
-    fourthWheelOffset = int(input("Enter the offset for the fourth wheel, numbers divisible by 26 will not encrypt.")) % 26
-
-    offsets = [firstWheelOffset, secondWheelOffset, thirdtWheelOffset, fourthWheelOffset]    
-
-    message = input("Enter the message you would like to encrypt.")
     #TODO: Remove spaces from the input
     message = message.upper()
     #TODO: verify string is alphabetical.
 
     encryptedMessage = ""
+    encryptedMessageJP = ""
     for character in message:
         characterAsInt = (ord(character) - 65) #interptet the character as a number
         wheelNumber = random.randint(0, 3)
         #access the number on a random wheel at the appropriate offset, add to the string.
 
         #debug help
-        print("Wheel: " + str(wheelNumber))
-        print("character: " + str(character))
-        print("As int: " + str(characterAsInt))
-        print("As int with offset: " + str((characterAsInt + (offsets[wheelNumber])) % 26))
-
+        #print("Wheel: " + str(wheelNumber))
+        #print("character: " + str(character))
+        #print("As int: " + str(characterAsInt))
+        #print("As int with offset: " + str((characterAsInt + (offsets[wheelNumber])) % 26))
         encryptedMessage += str(wheels[wheelNumber][(characterAsInt + (offsets[wheelNumber])) % 26]) + " "
-    print("your encrypted message is" + encryptedMessage)
+        encryptedMessageJP += symbolArrayJP[ALPHABET_SIZE * wheelNumber + (characterAsInt + (offsets[wheelNumber])) % 26] + " "
+    #print("your encrypted message is " + encryptedMessage)
+    #print("your encrypted message in JP is " + encryptedMessageJP)
+    print(encryptedMessageJP)
         
-def decrypt():
-    message = input("Enter the message you would like to decrypt")
-    firstWheelOffset = int(input("Enter the offset for the first wheel")) % 26
-    secondWheelOffset = int(input("Enter the offset for the second wheel")) % 26
-    thirdtWheelOffset = int(input("Enter the offset for the third wheel")) % 26
-    fourthWheelOffset = int(input("Enter the offset for the fourth wheel")) % 26
-    offsets = [firstWheelOffset, secondWheelOffset, thirdtWheelOffset, fourthWheelOffset]
+def decrypt(message, offsets):
+    for i in range(len(offsets)):
+        offsets[i] = offsets[i] % ALPHABET_SIZE
 
     decryptedMessage = ""
     #split the message up at the spaces an interpret each integer, matching it to the wheel.
     for symbol in message.split():
-        symbol = int(symbol)
-        wheel = int(symbol / ALPHABET_SIZE)
-        totalOffset = (symbol % ALPHABET_SIZE)
+        symbolIndex = symbolArrayJP.index(symbol)
+        wheel = int(symbolIndex / ALPHABET_SIZE)
+        totalOffset = (symbolIndex % ALPHABET_SIZE)
         correctedOffset = totalOffset - offsets[wheel]
         decypheredSymbol = correctedOffset % ALPHABET_SIZE
         decryptedMessage += chr(decypheredSymbol + 65)
-        
+
     print("your decrypted message is: " + decryptedMessage) 
 
+#TODO: Allow symbol array to map outputs to various charactersets to obfuscate the cypher.
+symbolArray = ""
+symbolArrayGr = ['Λ', 'Δ', 'ꭕ', '︎', 'Φ', 'ὰ', '♌', '♑', '♊', 'σ', 'Υ', 'ζ', '►', '︎', 'δ', 'ὺ', '♍', 'χ', 'Σ', 'φ', 'ά', 'ε', '♋', 'ꭃ', '︎', '︎', 'Η', 'Π', '︎', '︎', 'Α', 'Χ', 'γ', 'α', '♈', 'Ρ', 'ῶ', 'Γ', 'θ', '︎', '︎', 'Ψ', 'ξ', 'ἐ', 'ο', 'ψ', 'Ξ', 'Ζ', 'β', 'Ν', 'ꬻ', 'ὸ', '♎', 'Ι', 'ꭖ', 'ꭂ', 'ν', 'ω', 'Ὧ', '♊', 'κ', 'ꭍ', 'ꭀ', 'ὁ', 'ρ', '︎', 'λ', 'μ', 'η', '♏', 'ῼ', '︎', '♓', 'Ο', 'Ᾰ', 'Ὃ', 'ꭄ', 'ꭔ', 'Ε', 'Μ', 'ὴ', '⑅', 'Β', 'Κ', 'ꭁ', '︎', 'ᾳ', '⫷', '︎', 'ꬷ', '⫸', 'ὲ', '♐', 'Ω', '♒', 'Ἔ', 'τ', 'έ', 'υ', 'ι', 'ῐ', 'π', 'Τ', 'Θ']
+symbolArrayJP = ['ヤ', 'む', 'よ', 'ユ', 'ナ', 'ヲ', 'カ', 'ぬ', 'た', 'ス', 'ぎ', '五', 'マ', 'で', 'く', '円', 'ど', 'レ', 'み', 'ろ', 'モ', 'ま', 'ノ', 'が', 'う', 'れ', 'り', 'ミ', 'ソ', 'ぞ', 'あ', 'ぴ', 'タ', 'ム', 'し', 'を', 'チ', 'ゆ', 'め', 'え', 'ク', 'ハ', 'ウ', 'ず', 'さ', 'つ', 'け', 'す', '天', 'サ', 'も', 'メ', 'へ', 'ち', 'て', 'こ', 'な', 'ワ', 'ツ', 'コ', 'ラ', 'や', 'い', 'シ', '心', '山', 'ホ', '女', 'ほ', 'び', 'ヌ', 'オ', 'る', 'わ', 'ト', 'ぷ', 'そ', 'ぐ', 'テ', 'に', 'お', 'ネ', 'セ', 'せ', 'じ', 'ケ', 'ね', 'フ', 'ふ', 'は', 'か', 'リ', 'き', 'の', 'ら', 'と', '小', 'ひ', '田', 'キ', 'ヒ', 'ぺ', 'だ', 'ル']
 ALPHABET_SIZE = 26
 wheels = [
     [i for i in range(0, ALPHABET_SIZE)],
